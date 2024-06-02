83071a627d2ea9aa621e58927d7825c5dad87436
Jacob Reola <jacobreola@gmail.com>
2024-06-02 18:13:15 -0500

    updated

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,12 +2,16 @@
 
 import React from 'react'
 import { ThemeProvider } from './theme-provider';
+import { Toaster } from '@/components/ui/toaster';
 
 function Providers({children}:{children:React.ReactNode}) {
   return (
-    <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-        {children}
-    </ThemeProvider>
+    <>
+      <Toaster />
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+            {children}
+        </ThemeProvider>
+    </>
   )
 }
 
