29c97b9d4cb2ebc66a2df211294f818a367b1cce
Jacob Reola <jacobreola@gmail.com>
2024-06-06 05:49:13 -0500

    created rental

--- utils/actions.ts
+++ utils/actions.ts
@@ -4,6 +4,7 @@ import {
   imageSchema,
   profileSchema,
   validateWithZodSchema,
+  propertySchema,
 } from './schemas';
 import db from './db';
 import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
@@ -166,4 +167,19 @@ export const updateProfileImageAction = async (
   } catch (error) {
     return renderError(error);
   }
+};
+
+export const createPropertyAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  // const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(propertySchema, rawData);
+    return { message: 'propery created'}
+  } catch (error) {
+    return renderError(error);
+  }
+  // redirect('/');
 };
