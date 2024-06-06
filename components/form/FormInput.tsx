03a6c523752a028ddb9fadba2930de82e1132498
Jacob Reola <jacobreola@gmail.com>
2024-06-06 06:58:44 -0500

    description input

--- components/form/FormInput.tsx
+++ components/form/FormInput.tsx
@@ -18,7 +18,7 @@ function FormInput({
     placeholder,
 }: FormInputProps) {
     return (
-        <div className='mb-2'>
+        <div className='mb-'>
             {/* dynamic label and input because {}  */}
             <Label htmlFor={name} className='capitalize'>
                 {label || name}
